8ea5abd31c2cf771aeb6d48ab575c91a5cb150d5
damien911224 <damien911224@gmail.com>
2023-03-14 16:55:44 +0900

    Modified

--- React/model/setup.py
+++ React/model/setup.py
@@ -4,6 +4,9 @@ from torch.utils.cpp_extension import CUDA_HOME
 from torch.utils.cpp_extension import CppExtension
 from torch.utils.cpp_extension import CUDAExtension
 
+from setuptools import find_packages
+from setuptools import setup
+
 setup(
     name='React_extend',
     ext_modules=[
