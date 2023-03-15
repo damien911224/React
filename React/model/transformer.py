3629045f56ba8627f509ff9b8071c6f5f3e7a9d5
damien911224 <damien911224@gmail.com>
2023-03-15 22:23:55 +0900

    Modified

--- React/model/transformer.py
+++ React/model/transformer.py
@@ -32,7 +32,7 @@ class MLP(nn.Module):
 class Transformer(nn.Module):
 
     def __init__(self, num_class, d_model=512, nhead=8, encoder_sample_num=3, decoder_sample_num=4, num_encoder_layers=6,
-                 num_decoder_layers=6, dim_feedforward=2048, dropout=0.1, activation="prelu", normalize_before=False,
+                 num_decoder_layers=6, dim_feedforward=2048, dropout=0.1, activation="relu", normalize_before=False,
                  return_intermediate_dec=False):
         super().__init__()
 
